fb0860093d4f3c9702af95787e7d35e355ad8e71
MDS\nicholas.gre6280 <nikgreen496@gmail.com>
2019-05-01 12:03:25 +1200

    platforms part 1(falling and moving)

---
+++ Assets/Platform_Breaking.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform_Breaking : MonoBehaviour
+{
+    private bool m_bCollatsing = false;
+    public int m_intStanability;// howmany times the playey can land on it
+    public float m_fSpeed;
+    public float m_fTimer;
+    public float m_fMaxTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_bCollatsing){
+            if (m_fTimer <= 0)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                m_fTimer -= Time.deltaTime;
+            }
+           GetComponent<Rigidbody>().velocity = Vector3.down * m_fSpeed;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.tag);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            m_intStanability--;
+            if (m_intStanability == 0)
+            {
+                Copsate();
+                StartCoroutine(destoration());
+            }
+            else
+            {
+                //do effect
+            }
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+        }
+    }
+
+    private void Copsate()
+    {
+       
+    }
+    IEnumerator destoration()
+    {
+        Debug.Log("help");
+        //cumbling effect
+        yield return new WaitForSeconds(2);
+        m_fTimer = m_fMaxTimer;
+        m_bCollatsing = true;
+    }
+}
---
+++ Assets/movingPlatform.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingPlatform : MonoBehaviour
+{
+    public GameObject[] m_rPoints;
+    public int m_intCurrentPoint = 0;
+    public float m_fSpeed;
+    public float m_fPauseDuration;
+    public float m_fCurrentPause;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log(m_rPoints.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_fCurrentPause <= 0)
+        {
+            transform.position = Vector3.MoveTowards(gameObject.transform.position, m_rPoints[m_intCurrentPoint].transform.position, m_fSpeed);
+
+            if (Vector3.Distance(transform.position, m_rPoints[m_intCurrentPoint].transform.position) < 1)
+            {
+                if (m_rPoints.Length - 1 == m_intCurrentPoint)
+                {
+                    m_fCurrentPause = m_fPauseDuration;
+                    m_intCurrentPoint = 0;
+                }
+                else
+                {
+                    m_fCurrentPause = m_fPauseDuration;
+                    m_intCurrentPoint++;
+                }
+            }
+        }
+        else
+        {
+            m_fCurrentPause -= Time.deltaTime;
+        }
+        
+    }
+}
