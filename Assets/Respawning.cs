db8fd842c2d775052501e02b136e8cfe5c2d23ba
MDS\nicholas.gre6280 <nikgreen496@gmail.com>
2019-05-02 11:10:54 +1200

    inproving platform part 2 respawning platform which get destoryed

---
+++ Assets/Respawning.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawning : MonoBehaviour
+{
+    public enum eResetType
+    {
+        breaking,
+        breakingMoving
+    }
+
+   
+    public eResetType m_eResetType;
+    public float m_fTimer;
+    public float m_fMaxTimer;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Starting();
+    }
+
+    public void Starting()
+    {
+
+        if (m_fMaxTimer == 0)
+        {
+            m_fMaxTimer = 2;
+        }
+        m_fTimer = m_fMaxTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_fTimer <= 0)
+        {
+            Reset();
+        }
+        else
+        {
+            m_fTimer -= Time.deltaTime;
+        }
+    }
+
+    private void Reset()
+    {
+        switch (m_eResetType)
+        {
+            case eResetType.breaking:
+                gameObject.transform.position = gameObject.transform.parent.position;
+                gameObject.GetComponent<MeshRenderer>().enabled = true;
+                gameObject.GetComponent<PlatformBreaking>().enabled = true;
+                gameObject.GetComponent<PlatformBreaking>().Starting();
+
+                gameObject.GetComponent<Respawning>().enabled = false;
+                break;
+
+            case eResetType.breakingMoving:
+                gameObject.transform.position = gameObject.transform.parent.position;
+                gameObject.GetComponent<MeshRenderer>().enabled = true;
+                gameObject.GetComponent<MovingPlatform>().enabled = true;
+                gameObject.GetComponent<MovingPlatform>().m_intCurrentPoint = 0; 
+
+                gameObject.GetComponent<Respawning>().enabled = false;
+                break;
+            default:
+                break;
+        }
+    }
+}
--- Assets/movingPlatform.cs
+++ Assets/movingPlatform.cs
@@ -2,13 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movingPlatform : MonoBehaviour
+public class MovingPlatform : MonoBehaviour
 {
+    [Header("movement")]
     public GameObject[] m_rPoints;
     public int m_intCurrentPoint = 0;
     public float m_fSpeed;
+
+    [Header("delaying")]
     public float m_fPauseDuration;
     public float m_fCurrentPause;
+
+    [Header("destorying")]
+    public bool m_bDestoryAtPoint;
+    public int m_intBreakableAtPoint;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +30,8 @@ public class movingPlatform : MonoBehaviour
         {
             transform.position = Vector3.MoveTowards(gameObject.transform.position, m_rPoints[m_intCurrentPoint].transform.position, m_fSpeed);
 
+            
+            
             if (Vector3.Distance(transform.position, m_rPoints[m_intCurrentPoint].transform.position) < 1)
             {
                 if (m_rPoints.Length - 1 == m_intCurrentPoint)
@@ -34,6 +44,23 @@ public class movingPlatform : MonoBehaviour
                     m_fCurrentPause = m_fPauseDuration;
                     m_intCurrentPoint++;
                 }
+
+                if (m_bDestoryAtPoint)
+                {
+                    if (m_intBreakableAtPoint == m_intCurrentPoint)
+                    {
+                        if (gameObject.GetComponent<PlatformBreaking>() == null)
+                        {
+                            gameObject.AddComponent<PlatformBreaking>();
+                        }
+                        else
+                        {
+                            gameObject.GetComponent<PlatformBreaking>().enabled = true;
+                        }
+
+                        StartCoroutine(colapse());
+                    }
+                }
             }
         }
         else
@@ -41,5 +68,13 @@ public class movingPlatform : MonoBehaviour
             m_fCurrentPause -= Time.deltaTime;
         }
         
+    }
+
+    IEnumerator colapse()
+    {
+        
+        yield return new WaitForSeconds(.1f);
+        gameObject.GetComponent<PlatformBreaking>().Copsate();
+        gameObject.GetComponent<MovingPlatform>().enabled = false;
     }
 }
