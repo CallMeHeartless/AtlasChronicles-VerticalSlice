cea4b4d63c8c970746451ab9f5b570bd99c2d980
Kerry <k.of.blades@gmail.com>
2019-05-06 09:34:29 +1200

    Created (empty) player Audio controller - requires audio to be gathered

---
+++ Assets/Scripts/PlayerAudioController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudioController : AudioController
+{
+    public override void Start() {
+        base.Start();
+    }
+}
