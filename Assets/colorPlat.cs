541163edf627af3bbc3f1701e6df2d7fdd7472ea
MDS\nicholas.gre6280 <nikgreen496@gmail.com>
2019-05-08 09:51:18 +1200

    door though color  part1

--- Assets/colorPlat.cs
+++ Assets/colorPlat.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class colorPlat : MonoBehaviour
 {
+    public Material[] MaterialColor = new Material[5]; 
+    public GameObject effectingObject;
     public enum Colors
     {
         blue,green,pink,red,yellow
@@ -15,19 +17,59 @@ public class colorPlat : MonoBehaviour
     public string PassName;
     public Colors CurrentColor;
     public Colors setColor;
+    public match type;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponentInChildren<MeshRenderer>().material = MaterialColor[(sbyte)CurrentColor];
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            changeColor();
+        }
+    }
     // Update is called once per frame
-    void Update()
+    void CorrectColor()
     {
         if (setColor == CurrentColor)
         {
+            switch (type)
+            {
+                case match.door:
+                    //passName  TRUE
+                    break;
+                case match.lift:
+                    //passName  TRUE
+                    break;
+                case match.Switch:
+                    //passName  TRUE
+                    break;
+                default:
+                    break;
+            }
 
-        } 
+        }
+        else
+        {
+            switch (type)
+            {
+                case match.door:
+                    //passName FALSE
+                    break;
+                case match.lift:
+                    //passName FALSE
+                    break;
+                case match.Switch:
+                    //passName FALSE
+                    break;
+                default:
+                    break;
+            }
+
+        }
     }
 
     void changeColor()
@@ -41,5 +83,7 @@ public class colorPlat : MonoBehaviour
 
             CurrentColor += 1;
         }
+        GetComponentInChildren<MeshRenderer>().material = MaterialColor[(sbyte)CurrentColor];
+        CorrectColor();
     }
 }
