c34610d7ba4a5f36b54dad12d888b691c2cd7558
MDS\nicholas.gre6280 <nikgreen496@gmail.com>
2019-05-20 11:52:05 +1200

    moving platform Box Coll works Far better

--- Assets/Scripts/Obstacles/MovingPlatform.cs
+++ Assets/Scripts/Obstacles/MovingPlatform.cs
@@ -13,6 +13,14 @@ public class MovingPlatform : MonoBehaviour
     public float m_fPauseDuration;
     public float m_fCurrentPause;
 
+    [Header("testing Don't Touch")]
+    public float m_fPauseBreak = 0;
+    public bool m_fCurrentBreak = false;
+    public float timeITakes = 0;
+    public float jounlylength;
+    public float destionlength;
+
+
     [Header("shinking")]
     public bool Shink = false;
     public float ShinkSize = 1;
@@ -33,38 +41,67 @@ public class MovingPlatform : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         if (m_fCurrentPause <= 0)
         {
             transform.position = Vector3.MoveTowards(gameObject.transform.position, m_rPoints[m_intCurrentPoint].transform.position, m_fSpeed);
+            timeITakes += Time.deltaTime;
 
-            if (Shink)
+
+            if (m_fCurrentBreak == true)
             {
-                if (m_intCurrentPoint == 0)
+                if (m_fPauseBreak>= 0)
                 {
-                    box.size += new Vector3( 0.1f * ShinkSize * 2,0, 0);
-                    box.center += new Vector3(0.1f * ShinkSize,0, 0);
+
+
+                    if (m_intCurrentPoint == 0)
+                    {
+                        box.center += new Vector3(0.1f * ShinkSize, 0, 0);
+                    }
+                    else
+                    {
+                        box.center += new Vector3(-0.1f * ShinkSize, 0, 0);
+                    }
+                    m_fPauseBreak -= Time.deltaTime;
+
                 }
                 else
                 {
-                    box.size += new Vector3(-0.1f * ShinkSize * 2, 0, 0);
-                    box.center += new Vector3(-0.1f * ShinkSize, 0, 0);
+                    m_fCurrentBreak = false;
                 }
             }
-            
-            
-            if (Vector3.Distance(transform.position, m_rPoints[m_intCurrentPoint].transform.position) < 1)
+            else
             {
+                if (Shink)
+                {
+                    //shinking2();
+                    shinking();
+                }
+            }
+           
+
+
+                if (Vector3.Distance(transform.position, m_rPoints[m_intCurrentPoint].transform.position) < 1)
+            {
+                Debug.Log(timeITakes);
+                timeITakes = 0;
                 if (m_rPoints.Length - 1 == m_intCurrentPoint)
                 {
                     m_fCurrentPause = m_fPauseDuration;
                     m_intCurrentPoint = 0;
+                    box.size = new Vector3(1, 1, 1);
+                    box.center = new Vector3(0, 0, 0);
                 }
                 else
                 {
                     m_fCurrentPause = m_fPauseDuration;
                     m_intCurrentPoint++;
+                }
+
+                if (m_fPauseBreak>0)
+                {
+                    m_fCurrentBreak = true;
                 }
 
                 if (m_bDestoryAtPoint)
@@ -98,5 +135,69 @@ public class MovingPlatform : MonoBehaviour
         yield return new WaitForSeconds(.1f);
         gameObject.GetComponent<PlatformBreaking>().Copsate();
         gameObject.GetComponent<MovingPlatform>().enabled = false;
+    }
+
+    void shinking2(){
+        //gameObject.transform.localScale += new Vector3(0.1f * ShinkSize * 2, 0, 0);
+    }
+
+
+    void shinking()
+    {
+       
+            if (m_intCurrentPoint == 0)
+            {
+                if (-ShinkSize > 0)
+                {
+                   // Debug.Log("in");
+                    if (box.size.x+ ShinkSize >= 0)
+                    {
+                        //set to zero
+                        box.size += new Vector3(0.1f * ShinkSize * 2, 0, 0);
+                        box.center += new Vector3(0.1f * ShinkSize, 0, 0);
+                    }
+                    else
+                    {
+                        box.size = new Vector3(0, 1, 1);
+                        box.center += new Vector3(0.1f * ShinkSize, 0, 0);
+                        m_fPauseBreak += Time.deltaTime;
+                        
+                    }
+                }
+                else
+                {
+                    box.size += new Vector3(0.1f * ShinkSize * 2, 0, 0);
+                    box.center += new Vector3(0.1f * ShinkSize, 0, 0);
+
+                }
+
+            }
+            else
+            {
+                if (ShinkSize > 0)
+                {
+                    //Debug.Log("in");
+                    if (box.size.x+ShinkSize >= 0)
+                    {
+                        //set to zero
+                        box.size += new Vector3(-0.1f * ShinkSize * 2, 0, 0);
+                        box.center += new Vector3(-0.1f * ShinkSize, 0, 0);
+                    }
+                    else
+                    {
+                        box.size = new Vector3(0, 1, 1);
+                        box.center += new Vector3(-0.1f * ShinkSize, 0, 0);
+                        m_fPauseBreak += Time.deltaTime;
+                        
+                    }
+                }
+                else
+                {
+                    box.size += new Vector3(-0.1f * ShinkSize * 2, 0, 0);
+                    box.center += new Vector3(-0.1f * ShinkSize, 0, 0);
+
+                }
+            }
+        
     }
 }
