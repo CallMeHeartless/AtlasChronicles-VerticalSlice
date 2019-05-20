f6f0eb285f40680bf9ac036f42f655361da8df27
Kerry <k.of.blades@gmail.com>
2019-05-21 11:19:54 +1200

    Created DamageController - generic damage interface

---
+++ Assets/Scripts/System/MeleeAttack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeAttack : MonoBehaviour
+{
+    [SerializeField]
+    private int m_iDamage = 1;
+    [SerializeField]
+    private ParticleSystem m_rHitParticles;
+    [SerializeField]
+    private LayerMask m_HitLayer;
+    private Collider m_HitBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_HitBox = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
