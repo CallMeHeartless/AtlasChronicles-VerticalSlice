e85ce0aec8c112dce8e7a7986c793e6916a69720
MDS\nicholas.gre6280 <nikgreen496@gmail.com>
2019-05-21 12:01:19 +1200

    water and new respawnPoint Part1

---
+++ Assets/extraMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class extraMovement : MonoBehaviour
+{
+    public Vector3 Movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Movement;
+        //Movement = new Vector3(0,0,0);
+    }
+}
