8166d7e918e7915531a97396ebf39fae6ede9158
Vivian <seikyura@gmail.com>
2019-05-27 11:01:47 +1200

    WIP Breakable chest

---
+++ Assets/Scripts/Obstacles/BreakableObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableObject : MonoBehaviour
+{
+    [SerializeField] GameObject m_rBrokenObject;
+
+    public void SwitchToBroken()
+    {
+        if(m_rBrokenObject)
+        {
+            Instantiate(m_rBrokenObject, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
