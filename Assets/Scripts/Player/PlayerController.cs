88528a4b55f34b898a05353f5f2b0b6665cf4a6f
Kerry <k.of.blades@gmail.com>
2019-05-27 11:45:00 +1200

    tagged Goon weapon filter appropriately Player has respawn cue implemented for death

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -900,4 +900,13 @@ public class PlayerController : MonoBehaviour {
             TeleportToTeleportMarker();
         }
     }
+
+    // Force the player to Respawn
+    public void RespawnPlayer() {
+        GetComponent<DamageController>().ResetDamage();
+        GameObject respawnController = GameObject.Find("RespawnController");
+        if (respawnController) {
+            respawnController.GetComponent<RespawnController>().RespawnPlayer();
+        }
+    }
 }
