72d2240ab3657c63441e7887120fc828e08a284b
Kerry <k.of.blades@gmail.com>
2019-05-28 11:15:27 +1200

    Added IMessageReceiver interface, for sending more complicated messages around Updated the PlayerController to implement IMessageRecevier in order to check for a goon stealing a map fragment First pass at code to steal a map fragment from the player Refactored GameStats class

--- Assets/Scripts/System/GameStats.cs
+++ Assets/Scripts/System/GameStats.cs
@@ -6,7 +6,15 @@ public class GameStats {
     public static int[] s_iMapsBoard = new int[] { 0, 0, 0, 0};//4
     public static int[] s_iNoteBoard = new int[] { 0, 0, 0, 0};//4
     public static int s_iLevelIndex = 0;
-     
+    
+    // Steals a map fragment. Returns false if unable to do so
+    public static bool StealMapFragment() {
+        if(s_iMapsBoard[s_iLevelIndex] <= 0) {
+            return false;
+        }
+        --s_iMapsBoard[s_iLevelIndex];
+        return true;
+    }
     // Serialisation functions go here
 
 }
