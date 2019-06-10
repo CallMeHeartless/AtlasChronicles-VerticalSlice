ec5c164d0285c42d905aa13047a80bcf52102ec6
Vivian <seikyura@gmail.com>
2019-06-10 17:42:40 +1200

    Main menu now has settings. Need to adjust layout and also need to make controls screen

---
+++ Assets/Scripts/System/SelectOnInput.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectOnInput : MonoBehaviour {
+
+    [SerializeField] EventSystem eventSystem;
+    [SerializeField] GameObject selectedObject;
+
+    private bool bButtonSelected;
+    private AudioSource m_Next;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        eventSystem.SetSelectedGameObject(selectedObject);
+        bButtonSelected = true;
+
+        m_Next = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if((Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0) && !bButtonSelected)
+        {
+            eventSystem.SetSelectedGameObject(selectedObject);
+            bButtonSelected = true;
+        }
+
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            if (null != m_Next)
+                m_Next.Play();
+        }
+
+        if (Input.GetMouseButtonDown(0))
+            eventSystem.SetSelectedGameObject(selectedObject);
+    }
+
+    private void OnDisable()
+    {
+    }
+
+    private void OnEnable()
+    {
+        eventSystem.SetSelectedGameObject(selectedObject);
+        bButtonSelected = true;
+    }
+}
