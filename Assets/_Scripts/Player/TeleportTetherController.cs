b783432161650e8b2ee04d70ec9e0b6ff32a715d
Kerry <k.of.blades@gmail.com>
2019-08-27 11:01:08 +1200

    Changed the layer that the player model is on Started testing a visible teleport tether

---
+++ Assets/_Scripts/Player/TeleportTetherController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportTetherController : MonoBehaviour
+{
+    private LineRenderer m_LineRenderer;
+
+    // Start is called before the first frame update
+    void Start(){
+        // Obtain references
+        m_LineRenderer = GetComponent<LineRenderer>();    
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+    /// <summary>
+    /// Set the other end of of the teleport tether
+    /// </summary>
+    /// <param name="_position"></param>
+    public void SetTetherEnd(Vector3 _position) {
+        m_LineRenderer.SetPosition(1, _position);
+    }
+}
