bcf87d86d1298d9e330fb8837896f5e0492a19b9
MDS\nicholas.gre6280 <nikgreen496@gmail.com>
2019-10-11 11:06:54 +1300

    switchcnotrolller

--- Assets/_Scripts/Items/SwitchController.cs
+++ Assets/_Scripts/Items/SwitchController.cs
@@ -9,7 +9,7 @@ public class SwitchController : MonoBehaviour, IMessageReceiver
     private bool m_bTriggeredOnce = true;
 
     private DamageController m_rDamageController;
-    public GameObject OpenThisDoor;
+    public List<GameObject> OpenThisDoor;
     //[SerializeField]
     public List<MonoBehaviour> m_ObjectsToMessage;
 
@@ -54,7 +54,9 @@ public class SwitchController : MonoBehaviour, IMessageReceiver
     }
     public void switchoff()
     {
-        OpenThisDoor.GetComponent<Door>().SwitchChanged(0, true);
-
+        for (int i = 0; i < OpenThisDoor.Count; ++i)
+        {
+            OpenThisDoor[i].GetComponent<Door>().SwitchChanged(0, true);
+        }
     }
 }
